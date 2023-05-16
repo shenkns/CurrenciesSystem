ed4a8939b5c8d21e1ed04e20e0b905aff4425305
shenkns <shenkns@gmail.com>
2023-05-16 12:23:11 +0300

    Initial Commot

---
+++ Source/CurrenciesSystem/CurrenciesSystem.Build.cs
@@ -0,0 +1,43 @@
+// Copyright shenkns Currencies System Developed With Unreal Engine. All Rights Reserved 2023.
+
+using UnrealBuildTool;
+
+public class CurrenciesSystem : ModuleRules
+{
+	public CurrenciesSystem(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateIncludePaths.AddRange(
+			new string[] 
+			{
+				"CurrenciesSystem/Public/"
+			}
+		);
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core"
+			}
+		);
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"LogSystem"
+			}
+		);
+		
+		PrivateIncludePathModuleNames.AddRange(
+			new string[]
+			{
+				"LogSystem",
+				"DataSystem",
+				"SaveLoadSystem"
+			}
+		);
+	}
+}
